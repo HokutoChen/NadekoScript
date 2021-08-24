fa3570f5b48097009c5034cf4c4e3427bf4bca76
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-23 22:35:51 -0700

    updated fork

--- n-install.sh
+++ n-install.sh
@@ -13,7 +13,7 @@ rm -rf nadekobot_old 1>/dev/null 2>&1
 mv -fT nadekobot nadekobot_old 1>/dev/null 2>&1
 
 # clone new version
-git clone -b v3 --recursive --depth 1 https://github.com/HokutoChen/NadekoV3
+git clone -b v3 --recursive --depth 1 https://gitlab.com/Kwoth/nadekobot
 cd nadekobot
 
 # build
@@ -33,11 +33,9 @@ mv -f nadekobot_old/output/credentials.json nadekobot/output/credentials.json 1>
 # however, old user's strings will be backed up to strings_old
 
 # (C) backup new strings to reverse rewrite
-rm -rf nadekobot/output/data/strings_old 1>/dev/null 2>&1 # remove old backup preemptively to avoid copying what will get overwritten with new backup
 mv -f nadekobot/output/data/strings nadekobot/output/data/strings_new 1>/dev/null 2>&1
 
 # (B) backup new aliases to reverse rewrite
-rm -rf nadekobot/output/data/aliases_old.yml 1>/dev/null 2>&1
 mv -f nadekobot/output/data/aliases.yml nadekobot/output/data/aliases_new.yml 1>/dev/null 2>&1
 
 # (A) move old database
@@ -57,5 +55,5 @@ mv -rf nadekobot/output/data/strings nadekobot/output/data/strings_old 1>/dev/nu
 mv -rf nadekobot/output/data/strings_new nadekobot/output/data/strings 1>/dev/null 2>&1
 
 cd "$root"
-rm "$root/n-download.sh"
+rm "$root/n-install.sh"
 exit 0
