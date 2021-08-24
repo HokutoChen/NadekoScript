c8f65c139e56a393c953c81b33dfe83f68534e8f
Hokuto Chen <85286705+HokutoChen@users.noreply.github.com>
2021-08-23 23:39:45 -0700

    Update linuxAIO.sh

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -3,9 +3,11 @@ clear
 echo ""
 echo "Welcome to NadekoBot pi installer. Downloading the latest installer..."
 root=$(pwd)
+
+rm "$root/n-menu.sh" 1>/dev/null 2>&1
 wget -N https://github.com/HokutoChen/NadekoScript/raw/main/n-menu.sh
 
 bash n-menu.sh
 cd 
-rm $HOME/n-menu.sh
+rm $root/n-menu.sh
 exit 0
